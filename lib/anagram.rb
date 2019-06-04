14418cc799bc12cd35dc641c80d007de9bb72ec0
Nia Bert <nbert14@gmail.com>
2019-06-04 00:10:23 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+   def initialize(word)
+      @word = word
+   end
+  
+  def self.match(word_list)
+   word_array = word_list.split
+   word_array.each do |element| 
+    if element.split.sort == @word.sort
+      return element
+    else
+      puts []
+        end
+      end
+    end
+  
+end
